597e0606a184771715d794aaf4e9b72b0e54a863
avlima <albertovieiradelima@gmail.com>
2015-09-19 18:48:02 -0300

    translate blog

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">登录</div>
+				<div class="panel-heading">Login</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>哎呀!</strong> 输入错啦.<br><br>
+							<strong>Ops!</strong> Login inválido.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
@@ -22,14 +22,14 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">邮箱</label>
+							<label class="col-md-4 control-label">Email</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">密码</label>
+							<label class="col-md-4 control-label">Senha</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="password">
 							</div>
@@ -39,7 +39,7 @@
 							<div class="col-md-6 col-md-offset-4">
 								<div class="checkbox">
 									<label>
-										<input type="checkbox" name="remember"> 记住我
+										<input type="checkbox" name="remember"> lembrar
 									</label>
 								</div>
 							</div>
@@ -47,9 +47,9 @@
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">登录</button>
+								<button type="submit" class="btn btn-primary">Login</button>
 
-								<a class="btn btn-link" href="{{ url('/backend/password/email') }}">忘记密码?</a>
+								<a class="btn btn-link" href="{{ url('/backend/password/email') }}">esqueceu sua senha?</a>
 							</div>
 						</div>
 					</form>
